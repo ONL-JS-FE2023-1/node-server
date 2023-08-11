ab7b1f2e3031fff1f05617fdbfdd107702f93ef0
Roman Ratushnyi <freshcode1001@ukr.net>
2023-08-11 19:46:45 +0300

    Server response

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const http = require('http');
 const PORT = 5000;
 
 const requestListener = (req, res) => {
-    const {url, method} = req;
-    console.log(url, method);
+    // const {url, method} = req;
+    // console.log(url, method);
+    res.statusCode = 404;
+    res.end('Hello from server');
 }
 
 const server = http.createServer(requestListener);
