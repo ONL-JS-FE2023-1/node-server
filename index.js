020246ef2621650a485096e4b230adee1276d90e
Roman Ratushnyi <freshcode1001@ukr.net>
2023-08-14 18:25:33 +0300

    POST method reading user

--- index.js
+++ index.js
@@ -3,30 +3,52 @@ const fs = require("fs/promises");
 
 const PORT = 5000;
 
-const requestListener = async (req, res) => {
-  const { url } = req;
+const users = [];
 
-  if (url === "/index.html") {
-    try {
-      const data = await fs.readFile("./views/MainPage/index.html", "utf-8");
-      res.statusCode = 200;
-      res.end(data);
-    } catch (error) {
+const requestListener = async (req, res) => {
+  const { url, method } = req;
+
+  if(method === 'GET') {
+    if (url === "/index.html") {
+      try {
+        const data = await fs.readFile("./views/MainPage/index.html", "utf-8");
+        res.statusCode = 200;
+        res.end(data);
+      } catch (error) {
+        res.statusCode = 404;
+        res.end(error.message);
+      }
+    } else if (url === "/style.css") {
+      try {
+        const data = await fs.readFile("./views/style.css", "utf-8");
+        res.statusCode = 200;
+        res.end(data);
+      } catch (error) {
+        res.end(error.message);
+      }
+    } else {
       res.statusCode = 404;
-      res.end(error.message);
-    }
-  } else if (url === "/style.css") {
-    try {
-      const data = await fs.readFile("./views/style.css", "utf-8");
-      res.statusCode = 200;
+      const data = await fs.readFile("./views/NotFoundPage/index.html", "utf-8");
       res.end(data);
-    } catch (error) {
-      res.end(error.message);
+    }
+  } else if (method === 'POST') {
+    if(url === '/user') {
+      // тут ми напишемо логіку створення юзера
+      let jsonString = '';
+      req.on('data', (chunk) => {
+        jsonString += chunk;
+      })
+      req.on('end', () => {
+        const user = JSON.parse(jsonString);
+        users.push(user);
+        console.log(users);
+        res.statusCode = 201;
+        res.end();
+      })
     }
   } else {
-    res.statusCode = 404;
-    const data = await fs.readFile("./views/NotFoundPage/index.html", "utf-8");
-    res.end(data);
+    res.statusCode = 500;
+    res.end();
   }
 };
 
@@ -35,12 +57,4 @@ const server = http.createServer(requestListener);
 server.listen(PORT);
 
 // localhost:5000
-
-
-/*
-
-ДЗ.
-Можете спробувати зробити так, щоб користувач робив запит на різні стильові файли
-(в залежності, на якій він знаходить)
-
-*/
+// 127.0.0.1:5000
