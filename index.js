1f10d1d8c9e73fd38f18e667d58e7c154a408cbf
Roman Ratushnyi <freshcode1001@ukr.net>
2023-08-14 18:48:42 +0300

    Express welcome

--- index.js
+++ index.js
@@ -1,60 +1,15 @@
-const http = require("http");
-const fs = require("fs/promises");
+const express = require('express');
+
+const app = express();
 
 const PORT = 5000;
 
-const users = [];
+// спочатку ви описуєте роути сервера
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
 
-const requestListener = async (req, res) => {
-  const { url, method } = req;
-
-  if(method === 'GET') {
-    if (url === "/index.html") {
-      try {
-        const data = await fs.readFile("./views/MainPage/index.html", "utf-8");
-        res.statusCode = 200;
-        res.end(data);
-      } catch (error) {
-        res.statusCode = 404;
-        res.end(error.message);
-      }
-    } else if (url === "/style.css") {
-      try {
-        const data = await fs.readFile("./views/style.css", "utf-8");
-        res.statusCode = 200;
-        res.end(data);
-      } catch (error) {
-        res.end(error.message);
-      }
-    } else {
-      res.statusCode = 404;
-      const data = await fs.readFile("./views/NotFoundPage/index.html", "utf-8");
-      res.end(data);
-    }
-  } else if (method === 'POST') {
-    if(url === '/user') {
-      // тут ми напишемо логіку створення юзера
-      let jsonString = '';
-      req.on('data', (chunk) => {
-        jsonString += chunk;
-      })
-      req.on('end', () => {
-        const user = JSON.parse(jsonString);
-        users.push(user);
-        console.log(users);
-        res.statusCode = 201;
-        res.end();
-      })
-    }
-  } else {
-    res.statusCode = 500;
-    res.end();
-  }
-};
-
-const server = http.createServer(requestListener);
-
-server.listen(PORT);
-
-// localhost:5000
-// 127.0.0.1:5000
+// підіймаєте сервер з усіма роутами
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+})
