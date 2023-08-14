4a213630df3e4eaf8fe831450d47a1ff7b99cdfa
Roman Ratushnyi <freshcode1001@ukr.net>
2023-08-14 20:43:34 +0300

    Model + Controller + refactor

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-const express = require('express');
+// url, method = route, endpoint
 
-const app = express();
+const express = require('express')
 
-const PORT = 5000;
+const app = express()
+
+const PORT = 5000
+
+const bodyParser = express.json()
+
+const { validateUser } = require('./middlewares/index')
+
+const UserController = require('./controllers/UserController');
 
 // спочатку ви описуєте роути сервера
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
+app.post('/user', bodyParser, validateUser, UserController.createUser);
+
+// кожен учасник естафети - middleware (мідлвейр, мідлвар)
 
 // підіймаєте сервер з усіма роутами
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`)
 })
+
+/*
+
+Задача: реєстрацію юзера
+
+1. Прийняти запит на певний route (endpoint, ручка, маршрут...)
+2. JSON->JS
+3. Перевірити (валідувати) дані
+4. Зберегти ці дані (юзера)
+5. Надіслати користувачу відповідь
+
+*/
